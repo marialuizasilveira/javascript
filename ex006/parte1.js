fd3a6510ac5f5d32ac613e319bfc9c890ec35cc9
luizaas11 <130487258+marialuizasilveira@users.noreply.github.com>
2023-04-22 21:44:13 +0100

    Initial commit

---
+++ ex006/parte1.js
@@ -0,0 +1,7 @@
+var nota = 50
+console.log(`Sua nota final nesse ano foi de ${nota} pontos`)
+if (nota > 60) {
+    console.log('Você passou de ano! PARABÉNS!')
+} else {
+    console.log('Você foi REPROVADO!')
+}

14e9cda1c1cadcc1fe21568ad3383c209da36202
Maria Luiza <130487258+marialuizasilveira@users.noreply.github.com>
2025-11-21 16:42:39 +0000

    ajeitando

--- ex009/java.js
+++ ex009/java.js
@@ -5,22 +5,23 @@ function contar(){
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('Erro')
+        window.alert('Erro')  //caso algum campo esteja vazio
     } else {
         res.innerHTML = 'Contando: '
-        let i = Number(ini.value)
-        let f = Number(fim.value)
+        let i = Number(ini.value)  //value para pegar o valor digitado
+        let f = Number(fim.value) //Number para converter string em número
         let p = Number(passo.value)
-        if (p <= 0){
+
+        if (p <= 0){ //caso o passo seja 0 ou menor considear passo como 1
             window.alert('Passo Inválido! Considerando PASSO como 1!')
             p = 1
         }
         if (i < f){
-            for (let c = i; c <= f; c += p) {
+            for (let c = i; c <= f; c += p) {  //mostrar o contador maior pro menor
             res.innerHTML += `${c} \u{1f449}`
             }
         } else {
-            for (let c = i; c>= f; c-= p){
+            for (let c = i; c>= f; c-= p){  //mostar o contador menor pro maior
                res.innerHTML += `${c} \u{1f449}`
             }
         } res.innerHTML += '\u{1f3c1}'
--- ex010/java.js
+++ ex010/java.js
@@ -1,16 +1,17 @@
 function gerar(){
     let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
-    if (num.value.length == 0) {
+
+
+    if (num.value.length == 0) { //caso o campo esteja vazio
         window.alert('Por favor, digite um número!')
     } else {
-        let n = Number(num.value)
-        tab.innerHTML = ''
+        let n = Number(num.value) //converte o valor digitado em número e pego o valor escrito dentro do campo
+        tab.innerHTML = '' //limpa a tabuada antes de gerar uma nova
         for (c=1; c<=10; c++){
             let item = document.createElement('option')
             item.innerHTML = `${n} x ${c} = ${n*c}`
-            item.value = `tab${c}`
             tab.appendChild(item)
-        }
+        } //tabuada de 1 a 10. Cria um elemento option para colocar tabuada que sera criada.
     }
 }
